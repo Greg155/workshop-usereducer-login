1c4cfe7b19abfccb6a17ac4a105a91f49640d8ab
Greg155 <gregakhun@gmail.com>
2021-01-18 16:44:35 +0100

    update post workshop

--- src/App.js
+++ src/App.js
@@ -1,65 +1,103 @@
-import React, { useState } from 'react';
-import { login } from './utils/login';
+import React, { useState, useReducer } from "react";
+import { login } from "./utils/login";
 
-import './App.css'
+import "./App.css";
+import { act } from "react-dom/test-utils";
 
 const App = () => {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-  const [isLoading, setLoader] = useState(false);
-  const [error, setError] = useState('');
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const initalState = {
+    username: "",
+    password: "",
+    isLoading: false,
+    error: "",
+    isLoggedIn: false,
+  };
+
+  const [state, dispatch] = useReducer(reducer, initalState);
+  const { username, password, isLoading, error, isLoggedIn } = state;
 
   const onSubmit = async (e) => {
     e.preventDefault();
 
-    setError('');
-    setLoader(true);
+    dispatch({ type: "show_loader" });
 
     try {
-      await login({ username, password });
-      setIsLoggedIn(true);
+      console.log(username, password, "dans le try");
+      await login({ ...state });
+      dispatch({ type: "success" });
     } catch (error) {
-      setError('Incorrect username or password!');
-      setLoader(false);
-      setUsername('');
-      setPassword('');
+      console.log("oucou", error);
+      dispatch({ type: "error", payload: error });
     }
   };
 
+  function reducer(state, action) {
+    switch (action.type) {
+      case "fill_input":
+        return {
+          ...state,
+          [action.fieldName]: action.payload,
+        };
+      case "show_loader":
+        return { ...state, isLoading: true };
+      case "success":
+        return { ...state, isLoading: false, isLoggedIn: true };
+      case "error":
+        return { ...initalState, error: action.payload };
+      case "logout":
+        return { ...state, isLoggedIn: false };
+      default:
+        return state;
+    }
+  }
+
   return (
-    <div className='App'>
-      <div className='login-container'>
+    <div className="App">
+      <div className="login-container">
         {isLoggedIn ? (
           <>
             <h1>Welcome {username}</h1>
-            <button onClick={() => setIsLoggedIn(false)}>Log Out</button>
+            <button onClick={() => dispatch({ type: "logout" })}>
+              Log Out
+            </button>
           </>
         ) : (
-          <form className='form' onSubmit={onSubmit}>
-            {error && <p className='error'>{error}</p>}
+          <form className="form" onSubmit={onSubmit}>
+            {error && <p className="error">{error.toString()}</p>}
             <p>Please Login!</p>
             <input
-              type='text'
-              placeholder='username'
+              type="text"
+              placeholder="username"
               value={username}
-              onChange={(e) => setUsername(e.currentTarget.value)}
+              onChange={(e) =>
+                dispatch({
+                  type: "fill_input",
+                  fieldName: "username",
+                  payload: e.currentTarget.value,
+                })
+              }
             />
             <input
-              type='password'
-              placeholder='password'
-              autoComplete='new-password'
-              value={password}
-              onChange={(e) => setPassword(e.currentTarget.value)}
+              type="password"
+              placeholder="password"
+              autoComplete="new-password"
+              value={state.password}
+              onChange={(e) =>
+                dispatch({
+                  type: "fill_input",
+                  fieldName: "password",
+                  payload: e.currentTarget.value,
+                })
+              }
             />
-            <button className='submit' type='submit' disabled={isLoading}>
-              {isLoading ? 'Logging in...' : 'Log In'}
+            <button className="submit" type="submit" disabled={state.isLoading}>
+              {state.isLoading ? "Logging in ..." : "Log In"}
             </button>
           </form>
         )}
       </div>
     </div>
   );
-}
+};
 
-export default App
+export default App;
--- src/utils/login.js
+++ src/utils/login.js
@@ -1,11 +1,14 @@
 export async function login({ username, password }) {
-    return new Promise((resolve, reject) => {
-      setTimeout(() => {
-        if (username === 'pierre' && password === 'password') {
-          resolve();
-        } else {
-          reject();
-        }
-      }, 1000);
-    });
-  }
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (username === "pierre" && password === "password") {
+        console.log("dans le resolve");
+        resolve();
+      } else {
+        console.log("dans le reject");
+        console.log(username, password, "reject");
+        reject(new Error("Incorrect credientials"));
+      }
+    }, 1000);
+  });
+}
